fb23382409fd1265d52e809ad8d324798864a1f5
khanh16702 <ndkbb123123@gmail.com>
2023-05-08 01:14:13 +0700

    add map 3 and bonus lives

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -3,17 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class ItemCollector : MonoBehaviour
 {
 
 	[SerializeField] private Text cherriesText;
+    [SerializeField] private Text livesText;
 	[SerializeField] private AudioSource collectSoundEffect;
 
 	private void Start()
 	{
-		// Khi b?t ??u, ??t s? ?i?m hi?n t?i = s? ?i?m t?ng các vòng tr??c
-		PlayerPrefs.SetInt(GameConstant.currentScore, PlayerPrefs.GetInt(GameConstant.stageScore));
+        // Khi b?t ??u, ??t s? ?i?m hi?n t?i = s? ?i?m t?ng các vòng tr??c
+        PlayerPrefs.SetInt(GameConstant.currentScore, PlayerPrefs.GetInt(GameConstant.stageScore));
 		PlayerPrefs.Save();
         cherriesText.text = "Cherries: " + PlayerPrefs.GetInt(GameConstant.currentScore);
     }
@@ -40,6 +42,17 @@ public class ItemCollector : MonoBehaviour
             PlayerPrefs.SetInt(GameConstant.currentScore, PlayerPrefs.GetInt(GameConstant.currentScore) + 2);
             PlayerPrefs.Save();
             cherriesText.text = "Cherries: " + PlayerPrefs.GetInt(GameConstant.currentScore);
+        }
+
+        if (collision.gameObject.CompareTag("Apple"))
+        {
+            collectSoundEffect.Play();
+            Destroy(collision.gameObject);
+
+            // ?n táo t?ng m?ng lên 1
+            PlayerPrefs.SetInt(GameConstant.livesRest, PlayerPrefs.GetInt(GameConstant.livesRest) + 1);
+            PlayerPrefs.Save();
+            livesText.text = "Lives: " + PlayerPrefs.GetInt(GameConstant.livesRest);
         }
     }
 }
